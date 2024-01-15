ed1ccbb9a0eb9c0e14a391f0baa249502b6f7003
davis <zanxiaofeng@163.com>
2024-01-15 21:40:44 +0800

    init

---
+++ depends-jar-v1/src/main/java/zxf/common/Common.java
@@ -0,0 +1,41 @@
+package zxf.common;
+
+import java.util.Map;
+
+public class Common {
+    private String code;
+    private String message;
+    private Map<String, Object> properties;
+
+    public Common(){
+        System.out.println("Common in same-class-a loaded by " + this.getClass().getClassLoader());
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, Object> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Map<String, Object> properties) {
+        this.properties = properties;
+    }
+
+    static {
+        System.out.println("Static init of Common in same-class-a loaded by " + Common.class.getClassLoader());
+    }
+}
